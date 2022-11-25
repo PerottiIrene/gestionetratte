5a0e6e894f5ba6f4c7d539dbbe2cce5f3f59d919
PerottiIrene <ireneperotti29@gmail.com>
2022-11-25 16:25:47 +0100

    implementate classi repository

---
+++ src/main/java/it/prova/gestionetratte/repository/airbus/CustomAirbusRepositoryImpl.java
@@ -0,0 +1,15 @@
+package it.prova.gestionetratte.repository.airbus;
+
+import java.util.List;
+
+import it.prova.gestionetratte.model.Airbus;
+
+public class CustomAirbusRepositoryImpl implements CustomAirbusRepository{
+
+	@Override
+	public List<Airbus> findByExample(Airbus example) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ src/main/java/it/prova/gestionetratte/repository/tratta/CustomTrattaRepositoryImpl.java
@@ -0,0 +1,15 @@
+package it.prova.gestionetratte.repository.tratta;
+
+import java.util.List;
+
+import it.prova.gestionetratte.model.Tratta;
+
+public class CustomTrattaRepositoryImpl implements CustomTrattaRepository{
+
+	@Override
+	public List<Tratta> findByExample(Tratta example) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
